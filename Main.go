98c0d4386223ae82725789e619c797eae4c10bd2
Narkhan Kamzabek <narkhan2002@gmail.com>
2022-10-07 16:20:27 +0600

    As2

--- Main.go
+++ Main.go
@@ -1,10 +1,44 @@
 package main
 
+import "fmt"
+
 func main() {
-	hhkz := JobSite{}
-	Bob := NewPerson("Bob")
-	hhkz.addVacancy("Senior Golang Developer")
-	hhkz.subscribe(Bob)
-	hhkz.addVacancy("Junior Golang Developer")
-	hhkz.removeVacancy("Senior Golang Developer")
+	movie := NewMovie(LOTR{})
+	movie.PerformQuote()
+	movie.SetMovie(HarryPotter{})
+	movie.PerformQuote()
+}
+
+type QuoteReciter interface {
+	Recite()
+}
+
+type LOTR struct{}
+
+func (spm LOTR) Recite() {
+	fmt.Println("Not all those who wander are lost.” — Bilbo Baggins")
+}
+
+type HarryPotter struct{}
+
+func (sum HarryPotter) Recite() {
+	fmt.Println("If you can do one thing, hone it to perfection. Hone it to the utmost limit - Zenitsu")
+}
+
+type movie struct {
+	QuoteReciter QuoteReciter
+}
+
+func NewMovie(qr QuoteReciter) *movie {
+	return &movie{
+		QuoteReciter: qr,
+	}
+}
+
+func (t *movie) PerformQuote() {
+	t.QuoteReciter.Recite()
+}
+
+func (t *movie) SetMovie(qr QuoteReciter) {
+	t.QuoteReciter = qr
 }
